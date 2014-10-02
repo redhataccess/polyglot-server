900f2764328db5a12c1f4021e202fb0cd1e898d0
Connor Hindley <conn.hindley@gmail.com>
2014-10-02 15:25:28 -0400

    fallback to english during parse

--- parse.js
+++ parse.js
@@ -7,13 +7,18 @@ var glob = require('glob');
 require('./lib/db');
 var Q = require('q');
 var cache = require('memory-cache');
+var args = require('minimist')(process.argv.slice(2));
+var env = args.env || 'master';
 
 var Message = require('mongoose').model('Message');
 
 var messages = [],
+    englishObj = {},
+    englishDfd = Q.defer(),
     deferredList = [];
 
 function storeMessages() {
+    // Would like to use upsert, but hard to do in bulk
     Message.collection.remove({}, function(err) {
         if (err) {
             return console.log('Error dropping collection?');
@@ -25,6 +30,7 @@ function storeMessages() {
                 return;
             }
             console.log('Collection saved!');
+            // Nuke our cache.
             cache.clear();
             process.exit(0);
         });
@@ -39,11 +45,16 @@ function parseFile(file, lang) {
         if (err) {
             return console.error(err);
         }
+        if (lang === 'en') {
+            englishObj = obj;
+        }
         var m;
         for (m in obj) {
             messages.push({
                 key: m,
-                value: obj[m],
+                // Fallback to english string if this language doesn't have it.
+                // or an empty string in the worst case scenario
+                value: obj[m] || englishObj[m] || '',
                 lang: lang
             });
         }
@@ -52,19 +63,38 @@ function parseFile(file, lang) {
     return deferred.promise;
 }
 var DATA_DIR = (process.env.OPENSHIFT_DATA_DIR ? (process.env.OPENSHIFT_DATA_DIR) : './') + 'messages/';
-
-glob(DATA_DIR + '**/*.properties', function(err, files) {
+// We are going to parse the english messages first so they can be used as fallbacks in other languages.
+glob(DATA_DIR + '**/messages.properties', function(err, files) {
     if (err) {
         return console.error(err);
     }
-    var langRegEx = /\/messages_(\w{2,5})/;
-    files.forEach(function(file) {
-        var lang = 'en';
-        var matches = file.match(langRegEx);
-        if (matches && matches.length) {
-            lang = matches[1];
-        }
-        deferredList.push(parseFile(file, lang));
+    if (!files.length || files.length > 1) {
+        console.error('Wut.');
+        return;
+    }
+    parseFile(files[0], 'en').then(function() {
+        englishDfd.resolve();
     });
-    Q.all(deferredList).then(storeMessages);
 });
+englishDfd
+    .promise
+    .then(function() {
+        // We have the english strings, now we can work through the other languages.
+        glob(DATA_DIR + '**/*_*.properties', function(err, files) {
+            if (err) {
+                return console.error(err);
+            }
+            // regex to extract the lang from the file name
+            // an example is `messages_fr.properties`
+            var langRegEx = /\/messages_(\w{2,5})/;
+            files.forEach(function(file) {
+                var matches = file.match(langRegEx);
+                if (matches && matches.length) {
+                    // successful regex
+                    deferredList.push(parseFile(file, matches[1]));
+                }
+            });
+            // Wait until all messages have been parsed and then stuff them in the DB
+            Q.all(deferredList).then(storeMessages);
+        });
+    });
