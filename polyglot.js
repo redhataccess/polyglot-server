07a38d22b132d77da4ce7354e4569534f8db841d
Connor Hindley <conn.hindley@gmail.com>
2014-10-06 17:36:26 -0400

    Revert "IE CORS"
    This reverts commit ccb33ca4d24e903bdc53555b81109a75cffd033a.

--- polyglot.js
+++ polyglot.js
@@ -13,7 +13,7 @@
         FALLBACK_KEY = 'RHCP-_POLYGLOT',
         //STORAGE_KEY = 'RHCP-POLYGLOT',
         VALID_LANGS = ['en', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pt', 'ru', 'zh_CN'],
-        POLYGLOT_SERVER = 'https://polyglot-redhataccess.itos.redhat.com/',
+        POLYGLOT_SERVER = '//polyglot-redhataccess.itos.redhat.com/',
         hasStorage = ('localStorage' in window && window.localStorage !== null);
 
     /**
@@ -124,7 +124,7 @@
             queryData.version = version;
         }
 
-        this._get(POLYGLOT_SERVER, queryData, true).done(function(data) {
+        $.get(POLYGLOT_SERVER, queryData).done(function(data) {
             var keys = _objKeys(data),
                 prop;
 
@@ -189,25 +189,6 @@
                 _safeStore(FALLBACK_KEY, JSON.stringify(vals));
             }
         });
-    };
-
-    Polyglot.prototype._get = function(url, data, cors) {
-        var dfd = new $.Deferred(),
-            // *groan* IE
-            xhr = (typeof XDomainRequest !== 'undefined' && cors) ? new XDomainRequest() : new XMLHttpRequest();
-        xhr.onreadystatechange = function(data) {
-            if (xhr.readyState === 4) {
-                if (xhr.status < 400) {
-                    dfd.resolve(JSON.parse(data));
-                } else {
-                    dfd.reject();
-                }
-            }
-        }
-        var reqUrl = (url + '?' + $.param(data));
-        xhr.open('GET', reqUrl);
-        xhr.send(null);
-        return dfd.promise();
     };
 
     /**
